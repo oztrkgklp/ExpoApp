6e4e7c49a768e6582f1dcdb1bf265d682077b0b3
gokalp-ozturk-ai <gokalp.ozturk@ambeent.ai>
2021-12-09 15:29:27 +0300

    minor

--- frontend/desktop/src/renderer/src/components/Footer.js
+++ frontend/desktop/src/renderer/src/components/Footer.js
@@ -13,19 +13,6 @@ export default (props) => {
 
   return (
     <div>
-      <footer className="footer section py-5">
-        <Row>
-          <Col xs={12} lg={6} className="mb-4 mb-lg-0">
-            <p className="mb-0 text-center text-xl-left">
-              Copyright © 2021-{`${currentYear} `}
-              <Card.Link href="#" className="text-blue text-decoration-none fw-normal">
-                GARANTİ KONGRE
-              </Card.Link>
-            </p>
-          </Col>
-          
-        </Row>
-      </footer>
     </div>
   );
 };
